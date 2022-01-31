d112b63f8882d61d2bb88f998cbd264b0663a9eb
aaltw <awesthuis@move4mobile.com>
2022-01-31 17:34:53 +0100

    build: start of todo-to-pr

--- .github/workflows/todo-from-pr/index.js
+++ .github/workflows/todo-from-pr/index.js
@@ -8,4 +8,14 @@ try {
     // Read file from endpoints
 
     // npx nx run app:generate-todo --username=${{ secrets.CMS_USERNAME }} --pwd=${{ secrets.CMS_PASSWORD }} --endpoint=${{ secrets.CMS_ENDPOINT }}
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
 }
