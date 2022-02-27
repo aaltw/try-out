2579eddfc4f7cadbdd8de815440f90e5fb5444c8
aaltw <awesthuis@move4mobile.com>
2022-02-27 12:25:42 +0100

    chore: todo-to-pr index.js cleanup

--- .github/workflows/todo-from-pr/index.js
+++ .github/workflows/todo-from-pr/index.js
@@ -2,14 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
+
+    // Create a file
+
+    // Read file from endpoints
+
+    // npx nx run app:generate-todo --username=${{ secrets.CMS_USERNAME }} --pwd=${{ secrets.CMS_PASSWORD }} --endpoint=${{ secrets.CMS_ENDPOINT }}
 }
